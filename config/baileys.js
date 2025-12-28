7d3b1fb3e41d018cf8e228186af56c0e2dab8e51
paulfarayola2006 <paulfarayola2006@gmail.com>
2025-12-28 23:12:45 +0100

    a dev batch

--- config/baileys.js
+++ config/baileys.js
@@ -45,6 +45,7 @@ export const baileysConfig = {
   getMessage: defaultGetMessage,
   // version: [2, 3000, 1025190524], // remove comments if connection open but didn't connect on WhatsApp
   emitOwnEvents: true,
+  fireInitQueries: true,
   shouldIgnoreJid: (jid) => false,
   // Remove mentionedJid to avoid issues
   patchMessageBeforeSending: (msg) => {
